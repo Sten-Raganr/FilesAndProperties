343a3e06a584bc6cd8832d41209d81d663d1a958
Opilane TTHK <opilane@tthk.ee>
2020-11-16 15:04:31 +0200

    firstcommit

---
+++ FilesAndProperties/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace FilesAndProperties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rootPath = @"C:\Users\opilane\samples";
+            string txt = @"C:\Users\opilane\samples\filedata.txt";
+            string[] files = Directory.GetFiles(rootPath, "*.*", SearchOption.AllDirectories);
+            List<string> lines = new List<string>();
+            foreach(string file in files)
+            {
+                var filedata = new FileInfo(file);
+                string filename = filedata.Name;
+                string filedirectory = filedata.DirectoryName;
+                long filesize = filedata.Length;
+                //Console.WriteLine($"file name:{filename}; location:{filedirectory};size: {filesize} bytes");
+                string line= $"file name:{filename}; location:{filedirectory};size: {filesize} bytes";
+                lines.Add(line);
+                File.WriteAllLines(txt, lines);
+            }
+            Console.WriteLine("written!");
+        }
+    }
+}
